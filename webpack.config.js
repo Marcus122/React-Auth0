7398ed9020002d63d85bfb93dbf5b4ffbb63aeea
Marcus Atty <marcus.atty@gmail.com>
2016-07-23 23:23:43 +0100

    Config

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,16 @@
 var webpack = require("webpack");
 
 module.exports = {
-    context: __dirname + "/src",
-    entry: "./index.js",
+    context: __dirname,
+    entry: "./src/index.js",
     output: {
         path: __dirname + "dist",
         publicPath: 'assets/',
-	filename: "bundle.js"
+	    filename: "bundle.js"
     },
     module: {
         loaders: [
- 	    { test: /\.js?$/, loader: "babel"},	    
+ 	    { test: /\.js?$/, loader: "babel",exclude: /node_modules/},	    
 	    { test: /\.css$/, loader: "style-loader!css-loader" },
 	    { test: /\.less$/, loader: "style-loader!css-loader!less-loader" },
 	]
